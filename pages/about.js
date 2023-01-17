80fade21ca1d5297888cfcb53cce9880ecdb1b3b
Masvi <meetsjmasvi@hotmail.com>
2023-01-17 14:34:23 +0800

    Initial commit related to styling

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+import Styles from "../styles/about.module.scss";
+
+export default function About() {
+  return <div className={Styles.hightlight_scss}>About Page</div>;
+}
---
+++ pages/contact.js
@@ -0,0 +1,5 @@
+import Styles from "../styles/contact.module.scss";
+
+export default function Contact() {
+  return <div className={Styles.highlight_scss}>Contact Page</div>;
+}
